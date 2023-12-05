ed869e42e3ef8a6df0a0ab521a3f58df1e83377c
JECC-bit <56240699+JECC-bit@users.noreply.github.com>
2023-12-04 18:35:46 -0600

    Add files via upload

---
+++ assets/js/fun.js
@@ -0,0 +1,4 @@
+function guardar() {
+  var email1 = document.getElementById("email").Value;
+  alert(email1);
+}
---
+++ assets/js/index.js
@@ -0,0 +1,10 @@
+const menuHamburger = document.querySelector(".menu-hamburger");
+const navLinks = document.querySelector(".nav-links");
+
+menuHamburger.addEventListener("click", () => {
+  navLinks.classList.toggle("mobile-menu");
+});
+
+function hola() {
+  alert("sjidcnbis");
+}
